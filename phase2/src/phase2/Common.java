c76df38ef19330b1a0f2d80888bcdb83f05adf89
mfirdous <mfirdous@mail.bradley.edu>
2020-11-29 16:56:57 -0600

    Create Common.java

---
+++ phase2/src/phase2/Common.java
@@ -0,0 +1,41 @@
+package phase2;
+
+/**
+ * @author #04 | Moom Firdous 
+ * @author #10 | Meridith Kcoh 
+ * @author #12 | James Mckean
+ * @author #15 | Logan Ross
+ */
+public class Common {
+
+    public static int step = 0;
+    public static String indent1 = "   | ";
+    public static String indent2 = "       |";
+    public static String padding = "\n| ----------------------------- ";
+
+    public static enum Role {
+        SENDER, RECEIVER
+    }
+
+    public static String caseSeperator(String token, String testCase) {
+
+        String result = "";
+
+        for (int i = 0; i < testCase.length(); i++) {
+
+            result += token;
+        }
+   
+        result += "\n\n" + testCase + "\n\n";
+
+        for (int i = 0; i < testCase.length(); i++) {
+
+            result += token;
+        }
+        result += "\n";
+
+        return result;
+
+    }
+
+}
