fdc3f8ef6c322571ce80a5340d75a266eeedb166
mfirdous <mfirdous@mail.bradley.edu>
2020-11-29 16:57:02 -0600

    Create Cryptography.java

---
+++ phase2/src/phase2/Cryptography.java
@@ -0,0 +1,58 @@
+package phase2;
+
+import java.math.BigInteger;
+import static phase2.Common.*;
+
+/**
+ * @author #04 | Moom Firdous 
+ * @author #10 | Meridith Kcoh 
+ * @author #12 | James Mckean
+ * @author #15 | Logan Ross
+ */
+public class Cryptography {
+
+    private RSA rsa;
+
+    Cryptography() {
+
+        System.out.println(indent2 + "Run Cryptography --------");
+    
+        rsa = new RSA ();        
+        System.out.println(indent2 + rsa.toString());
+    
+    }
+
+   
+    // Use the mapping provided in our ICE, and you can modify the header of this method
+    static public BigInteger CBC(BigInteger msg, BigInteger hashBase) {   
+        //TODO
+        return BigInteger.ZERO;
+    }
+
+    static public BigInteger hash(BigInteger msg, BigInteger hashBase) {
+        BigInteger msgCopy = msg;
+        BigInteger hashValue = msgCopy.mod(hashBase);
+        return hashValue;
+    }
+
+    static public BigInteger shift(BigInteger msg, BigInteger ks) {
+        return msg.add(ks);
+    }
+
+    //(n, e)
+    public void getPublicKey(BigInteger[] pubKey) {
+       
+        pubKey[0] = rsa.getN();
+        pubKey[1] = rsa.getE();
+     
+    }
+
+    // (n, d)
+    public void getPrivateKey(BigInteger[] privateKey) {
+       
+       privateKey[0]= rsa.getN();
+       privateKey[1]= rsa.getD();
+       
+    }
+
+}
